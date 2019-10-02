59c688ac1a0714fee3939dece7e0642beb49e6f5
Danielandro <mtme13@hotmail.com>
2019-10-02 15:35:28 +0100

    Add default todos to App state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todos from './components/Todos';
 import './App.css';
 
 function App() {
+  const DefaultTodos = [
+    {
+      id: 1,
+      title: 'Complete homework',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Lived a life',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Walked to the farm',
+      completed: false
+    }
+  ]
+
+  const [todos] = useState(DefaultTodos)
+
   return (
     <div className="App">
       <h1>Welcome To React</h1>
       <Todos />
+      <ul>{todos.map((todo) => <li>{todo.title}</li>)}</ul>
     </div>
   );
 }
