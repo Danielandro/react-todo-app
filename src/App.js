745fe5db74c6fe46a8238bb05d996aee0582516d
Danielandro <mtme13@hotmail.com>
2019-10-02 15:18:17 +0100

    Create basic Todos component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome To React</h1>
+      <Todos />
     </div>
   );
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { directive } from '@babel/types';
+
+function Todos() {
+  return (
+    <div>
+      <h2>Todos</h2>
+    </div>
+  )
+}
+
+export default Todos;
