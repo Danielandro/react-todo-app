3281c5097ded769b52b5734218967d7d7b877ad4
Danielandro <mtme13@hotmail.com>
2019-10-06 03:05:58 +0100

    Add Home & About links to homepage

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
       <h1>About</h1>
       <p>This is the TodoList app v1.0.0. Part of a React crash course by
           <a href="https://www.youtube.com/watch?v=sBws8MSXN7A&t=3072s">
-          Brad Traversy
+          {' '}Brad Traversy
           </a>
       </p>
     </>
