2fcd5c316a6661ced82cf5d24df574ece15780db
Danielandro <mtme13@hotmail.com>
2019-10-12 23:45:50 +0100

    Test AddTodo component

--- src/components/todos/AddTodo.test.js
+++ src/components/todos/AddTodo.test.js
@@ -3,11 +3,15 @@ import { shallow } from 'enzyme';
 import AddTodo from './AddTodo';
 
 function setupShallow() {
-  const addTodo = jest.fn();
-  const enzymeWrapper = shallow(<AddTodo addTodo={addTodo} />)
+  const props = {
+    addTodo: jest.fn()
+  }
+
+  const enzymeWrapper = shallow(<AddTodo {...props} />)
 
   return {
-    enzymeWrapper
+    enzymeWrapper,
+    props
   }
 }
 
@@ -29,11 +33,25 @@ describe('AddTodo', () => {
     const { enzymeWrapper } = setupShallow();
     const inputField = enzymeWrapper.find('input[type="text"]');
 
-
     inputField.simulate('change', {
       target: { value: 'Wash the car' }
     });
 
     expect(enzymeWrapper.find('input[type="text"]').props().value).toEqual('Wash the car');
+  })
+
+  it('new todos are submitted', () => {
+    const { enzymeWrapper, props } = setupShallow();
+    const inputField = enzymeWrapper.find('input[type="text"]');
+
+    inputField.simulate('change', {
+      target: { value: 'Wash the car' }
+    });
+
+    enzymeWrapper.find('form').simulate('submit', {
+      preventDefault: () => { }
+    });
+
+    expect(props.addTodo).toHaveBeenCalledWith('Wash the car');
   })
 })
